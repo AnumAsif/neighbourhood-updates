d82ef07b8c65ce50f53c471b9309896877616e79
Anum Asif <anum@cockar.com>
2019-03-24 23:42:38 +0300

    Add tests for business class in database

--- myneighbourhood/tests.py
+++ myneighbourhood/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Profile, Location, Neighbourhood
+from .models import Profile, Location, Neighbourhood, Business
 from django.db import models
 # Create your tests here.
 class ProfileTestCase(TestCase):
@@ -50,6 +50,36 @@ class NeighbourhoodTestCase(TestCase):
 
     def test_delete_neighbourhood(self):
         self.neighbourhood.save()
-        self.delete_neighbourhood(neighbourhood.id)
-        hood=Neighbourhood.objects.filter(id=neighbourhood_id).first()
-        self.assertTrue(len(hood)==0)        
+        Neighbourhood.delete_neighbourhood(self.neighbourhood.id)
+        hood=Neighbourhood.objects.filter(id=self.neighbourhood.id).first()
+        self.assertTrue(hood==None)        
+
+    def test_update_occupants(self):
+        self.neighbourhood.save()
+        Neighbourhood.update_occupants(self.neighbourhood.id, 1800)
+        hood=Neighbourhood.objects.filter(id=self.neighbourhood.id).first()
+        self.assertTrue(hood.occupants_count==1800)
+     
+     
+class BusinessTestCase(TestCase):
+    def setUp(self):
+        self.owner = User.objects.create(username='user1')
+        self.location=Location.objects.create(name='Nairobi')
+        self.neighbourhood=Neighbourhood.objects.create(name="Lavington",occupants_count=2000,admin=self.owner,location=self.location) 
+        self.business=Business.objects.create(name='Barber', owner=self.owner,neighbourhood=self.neighbourhood, email="anum@yahoo.com")
+
+    
+    def tearDown(self):    
+        self.owner.delete()
+        self.location.delete()
+        self.neighbourhood.delete()
+        self.business.delete()
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business, Business)) 
+
+    def test_save_busioness(self):
+        self.business.save_business()
+        business=Business.objects.all()
+        self.assertTrue(len(business)>0)
+    
