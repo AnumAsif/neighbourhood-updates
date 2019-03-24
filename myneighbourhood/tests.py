d2c4d192e067c8b06c9d08639c8208fd84a8f50a
Anum Asif <anum@cockar.com>
2019-03-24 12:08:27 +0300

    Add tests for the neighbourhood class

--- myneighbourhood/tests.py
+++ myneighbourhood/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Location, Neighbourhood
 from django.db import models
 # Create your tests here.
 class ProfileTestCase(TestCase):
@@ -31,9 +31,25 @@ class ProfileTestCase(TestCase):
         self.assertTrue(profile!=None)
 
 # Create your tests here.
-def NeighbourhoodTestCase(TestCase):
+class NeighbourhoodTestCase(TestCase):
     def setUp(self):
         self.admin = User.objects.create(username='user1')
-        self.loation=Location.objects.create(location='Nairobi')
+        self.location=Location.objects.create(name='Nairobi')
+        self.neighbourhood=Neighbourhood.objects.create(name="Lavington",occupants_count=2000,admin=self.admin,location=self.location) 
         
     def tearDown(self):    
+        self.admin.delete()
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighbourhood, Neighbourhood)) 
+
+    def test_save_hood(self):
+        self.neighbourhood.save()
+        hoods=Neighbourhood.objects.all()
+        self.assertTrue(len(hoods)>0)
+
+    def test_delete_neighbourhood(self):
+        self.neighbourhood.save()
+        self.delete_neighbourhood(neighbourhood.id)
+        hood=Neighbourhood.objects.filter(id=neighbourhood_id).first()
+        self.assertTrue(len(hood)==0)        
